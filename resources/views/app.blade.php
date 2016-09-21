7f0ff13eedcfbe747459e1cbb1601f7aa473618a
somratcste <somratcste@gmail.com>
2016-09-21 13:03:58 +0600

    Add menu in collapse

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,31 +5,38 @@
   <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <!--Import materialize.css-->
   <link type="text/css" rel="stylesheet" href="{{ asset('css/materialize.min.css') }}"  media="screen,projection"/>
+  <link type="text/css" rel="stylesheet" href="{{ asset('css/style.css') }}"  media="screen,projection"/>
 
   <!--Let browser know website is optimized for mobile-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 
 <body>
-<div class="navbar-fixed">
-  <nav>
-  <div class="nav-wrapper">
-    <a href="#!" class="brand-logo">To Do List</a>
-    <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-    <ul class="right hide-on-med-and-down">
-      <li><a href="sass.html">Sass</a></li>
-      <li><a href="badges.html">Components</a></li>
-      <li><a href="collapsible.html">Javascript</a></li>
-      <li><a href="mobile.html">Mobile</a></li>
-    </ul>
-    <ul class="side-nav" id="mobile-demo">
-      <li><a href="sass.html">Sass</a></li>
-      <li><a href="badges.html">Components</a></li>
-      <li><a href="collapsible.html">Javascript</a></li>
-      <li><a href="mobile.html">Mobile</a></li>
-    </ul>
-  </div>
+<div class="container">
+ <ul id="slide-out" class="side-nav">
+    <li><div class="userView">
+      <img class="background" src="{{ asset('img/technology.jpg') }}">
+      <a href="#!user"><img class="circle" src="{{ asset('img/somrat.jpg') }}"></a>
+      <a href="#!name"><span class="black-text name">G. M. Nazmul Hossain Somrat</span></a>
+      <a href="#!email"><span class="black-text email">eng.nazmulhossain@gmail.com</span></a>
+    </div></li>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Task List</a></li>
+    <li><div class="divider"></div></li>
+    <li><a href="http://www.somrat.info/">ABOUT ME</a></li>
+  </ul>
+  
+   <nav>
+    <div class="nav-wrapper">
+    <a href="#" data-activates="slide-out" class="button-collapse" style="
+    display: -webkit-box;"><i class="material-icons">menu</i></a>
+    <a href="#!" class="brand-logo center">To Do List</a>
+    </div>
   </nav>
+ 
+</div>
+<div class="container">
+  
 </div>
 
 
